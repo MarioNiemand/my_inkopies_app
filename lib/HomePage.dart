e24612b8951c7418f1ec434801aef6d7084f6ea0
Mario Niemand <mario@idealsolution.co.za>
2021-07-23 15:29:31 +0200

    added files

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -2,7 +2,16 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:my_inkopies_app/food_data.dart';
+import 'package:flutter_launcher_icons/android.dart';
+import 'package:flutter_launcher_icons/constants.dart';
+import 'package:flutter_launcher_icons/custom_exceptions.dart';
+import 'package:flutter_launcher_icons/ios.dart';
+import 'package:flutter_launcher_icons/main.dart';
+import 'package:flutter_launcher_icons/utils.dart';
+import 'package:flutter_launcher_icons/xml_templates.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -28,11 +37,11 @@ List itemPriceList = [
   55
 ];
 List itemIconList = [
-  Icons.add,
-  Icons.phone_android,
-  Icons.mail,
-  Icons.cloud,
-  Icons.camera
+  'assets/icons/Fruits and Vegetables/apple.svg',
+  'assets/icons/Fruits and Vegetables/apple.svg',
+  'assets/icons/Fruits and Vegetables/apple.svg',
+  'assets/icons/Fruits and Vegetables/apple.svg',
+  'assets/icons/Fruits and Vegetables/apple.svg'
 ];
 List itemCountList = [
   1,
@@ -48,6 +57,7 @@ List itemChecked = [
   false,
   false
 ];
+
 
 AlertDialog alert = AlertDialog(
   title: Text("!"),
@@ -134,155 +144,168 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Stack(
         children: [
-          SingleChildScrollView(
-            physics: ScrollPhysics(),
-            child: Column(
-              children: [
-                ListView.builder(
-                  physics: NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  itemCount: itemNameList.length,
-                  itemBuilder: (context, index){
+          Padding(
+            padding: const EdgeInsets.only(bottom: 80),
+            child: SingleChildScrollView(
+              physics: ScrollPhysics(),
+              child: Column(
+                children: [
+                  ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    //itemCount: itemNameList.length,
+                    itemCount: itemNameList.length == null ? 0 : itemNameList.length,
+                    itemBuilder: (context, index){
 
-                    return  GestureDetector(
-                      onTap: () => showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                          title: Text('Update ${itemNameList[index]} ?'),
-                          content: SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.16,
-                            child: Form(
-                              onChanged: () {
-                                Form.of(primaryFocus!.context!)!.save();
-                              },
-                              child: Column(
-                                children: [
-                                  Row(
-                                    children: [
-                                      SizedBox(
-                                          width: MediaQuery.of(context).size.width * 0.5,
-                                          child: TextFormField(
-                                            initialValue: itemCountList[index].toString(),
-                                            decoration: const InputDecoration(
-                                              icon: Icon(Icons.format_list_numbered),
-                                              labelText: 'Hoeveelheid',
-                                            ),
-                                            onSaved: (String? amountvalue) {
-                                              itemCountList[index] = int.parse('$amountvalue');
-                                            },
-                                          )
-                                      ),
-                                    ],
-                                  ),
+                      return  GestureDetector(
+                        onTap: () => showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: Text('Update ${itemNameList[index]} ?'),
+                            content: SizedBox(
+                              height: MediaQuery.of(context).size.height * 0.16,
+                              child: Form(
+                                onChanged: () {
+                                  Form.of(primaryFocus!.context!)!.save();
+                                },
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                            width: MediaQuery.of(context).size.width * 0.5,
+                                            child: TextFormField(
+                                              initialValue: itemCountList[index].toString(),
+                                              decoration: const InputDecoration(
+                                                icon: Icon(Icons.format_list_numbered),
+                                                labelText: 'Hoeveelheid',
+                                              ),
+                                              onSaved: (String? amountvalue) {
+                                                itemCountList[index] = int.parse('$amountvalue');
+                                              },
+                                            )
+                                        ),
+                                      ],
+                                    ),
 
-                                  Row(
-                                    children: [
-                                      SizedBox(
-                                          width: MediaQuery.of(context).size.width * 0.5,
-                                          child: TextFormField(
-                                            initialValue: itemPriceList[index].toString(),
-                                            decoration: const InputDecoration(
-                                              icon: Icon(Icons.money),
-                                              labelText: 'Prys',
-                                            ),
-                                            onSaved: (String? myvalue) {
-                                              itemPriceList[index] = int.parse('$myvalue');
-                                            },
-                                          )
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                                    Row(
+                                      children: [
+                                        SizedBox(
+                                            width: MediaQuery.of(context).size.width * 0.5,
+                                            child: TextFormField(
+                                              initialValue: itemPriceList[index].toString(),
+                                              decoration: const InputDecoration(
+                                                icon: Icon(Icons.money),
+                                                labelText: 'Prys',
+                                              ),
+                                              onSaved: (String? myvalue) {
+                                                itemPriceList[index] = int.parse('$myvalue');
+                                                //TODO: save stored price locally
+                                                // final mylist = loadFoodData();
+                                                // final FoodData listItem = mylist[index];
+                                                // listItem.price = int.parse('$myvalue');
+                                              },
+                                            )
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: (){
+                                  //Form.of(primaryFocus!.context!)!.save();
+                                  Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
+                                } ,
+                                child: const Text('Save'),
+                              ),
+                              TextButton(
+                                onPressed: (){
+                                  Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
+                                },
+                                child: const Text('Cancel'),
+                              ),
+                            ],
                           ),
-                          actions: <Widget>[
-                            TextButton(
-                              onPressed: (){
-                                //Form.of(primaryFocus!.context!)!.save();
-                                Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
-                              } ,
-                              child: const Text('Save'),
-                            ),
-                            TextButton(
-                              onPressed: (){
-                                Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
-                              },
-                              child: const Text('Cancel'),
-                            ),
-                          ],
                         ),
-                      ),
-                      child: Card(
-                        child: Row(
-                          children: [
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.83,
-                              child: ListTile(
-                                title: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Row(
-                                    children: [
-                                      Icon(itemIconList[index]),
-                                      Padding(
-                                        padding: const EdgeInsets.only(left: 15),
-                                        child: Column(
-                                          crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: [
-                                            Text('${itemNameList[index]}'),
-                                            Text('R ${itemPriceList[index]}'),
-                                          ],
+                        child: Card(
+                          child: Row(
+                            children: [
+                              SizedBox(
+                                width: MediaQuery.of(context).size.width * 0.83,
+                                child: ListTile(
+                                  title: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Row(
+                                      children: [
+                                        SizedBox(width: MediaQuery.of(context).size.width / 10 ,height: MediaQuery.of(context).size.height / 10,
+                                          child:  SvgPicture.asset(
+                                              itemIconList[index]
+                                          ),
                                         ),
-                                      ),
 
-                                      Expanded(
-                                        child: Padding(
-                                          padding: const EdgeInsets.only(left: 5),
+                                        Padding(
+                                          padding: const EdgeInsets.only(left: 15),
                                           child: Column(
-                                            crossAxisAlignment: CrossAxisAlignment.end,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
                                             children: [
-
-                                              Text('x${itemCountList[index]}'),
+                                              Text('${itemNameList[index]}'),
+                                              Text('R ${itemPriceList[index]}'),
                                             ],
                                           ),
                                         ),
-                                      ),
-                                    ],
+
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(left: 5),
+                                            child: Column(
+                                              crossAxisAlignment: CrossAxisAlignment.end,
+                                              children: [
+
+                                                Text('x${itemCountList[index]}'),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
                                   ),
+
                                 ),
-
                               ),
-                            ),
-                            SizedBox(
+                              SizedBox(
 
-                              width: MediaQuery.of(context).size.width / 8,
-                              child: Checkbox(
-                                value: itemChecked[index],
-                                onChanged: (bool? value) {
-                                  setState(() {
-                                    if(itemChecked[index] == true){
-                                      itemChecked[index] = false;
-                                    }else{
-                                      itemChecked[index] = true;
-                                    }
-                                  });
-                                },
+                                width: MediaQuery.of(context).size.width / 8,
+                                child: Checkbox(
+                                  value: itemChecked[index],
+                                  onChanged: (bool? value) {
+                                    setState(() {
+                                      if(itemChecked[index] == true){
+                                        itemChecked[index] = false;
+                                      }else{
+                                        itemChecked[index] = true;
+                                      }
+                                    });
+                                  },
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
+
+
                         ),
-
-
-                      ),
-                    );
-                  },
-                ),
+                      );
+                    },
+                  ),
 
 
 
-              ],
+                ],
+              ),
+
             ),
-
           ),
           SizedBox(
             height: MediaQuery.of(context).size.height * 100,
@@ -294,21 +317,21 @@ class _HomePageState extends State<HomePage> {
                   child:
                   Card(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)
+                        borderRadius: BorderRadius.circular(50)
                     ),
                     elevation: 50,
                     child: SizedBox(
                       width: MediaQuery.of(context).size.width * 0.75,
                       child: ListTile(
                         leading: Text(
-                            "Total",
-                            style: TextStyle(
+                          "Total",
+                          style: TextStyle(
                               fontSize: 20
-                            ),
+                          ),
                         ),
 
                         trailing: Text(
-                            'R ${calculateTotal()}',
+                          'R ${calculateTotal()}',
                           style: TextStyle(
                               fontSize: 20
                           ),
@@ -362,7 +385,7 @@ class FoodItemsSearch extends SearchDelegate<FoodData>{
     itemChecked.add(false);
     itemPriceList.add(0);
     itemCountList.add(1);
-    itemIconList.add(Icon(Icons.texture));
+    itemIconList.add('assets/icons/Beverages/apple-juice.svg');
     throw UnimplementedError();
   }
 
@@ -386,7 +409,11 @@ class FoodItemsSearch extends SearchDelegate<FoodData>{
               Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
             },
             child: ListTile(
-              leading: Icon(listItem.icon),
+              leading: SizedBox(width: MediaQuery.of(context).size.width / 10 ,height: MediaQuery.of(context).size.height / 10,
+                child:  SvgPicture.asset(
+                    itemIconList[index]
+                ),
+              ),
               subtitle: Text(listItem.price.toString()),
               title: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -406,10 +433,14 @@ class FoodItemsSearch extends SearchDelegate<FoodData>{
               itemPriceList.add(listItem.price);
               Navigator.push( context, MaterialPageRoute( builder: (context) => HomePage()),);
 
-              },
+            },
 
             child: ListTile(
-              leading: Icon(listItem.icon),
+              leading: SizedBox(width: MediaQuery.of(context).size.width / 10 ,height: MediaQuery.of(context).size.height / 10,
+                child:  SvgPicture.asset(
+                    itemIconList[index]
+                ),
+              ),
               title: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -424,5 +455,5 @@ class FoodItemsSearch extends SearchDelegate<FoodData>{
     );
   }
 
-  
+
 }
